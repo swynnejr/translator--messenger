f8e41c8d9a4c9d58ea470fec21f3d2aefaf67c15
Sam Wynne <saucersam@gmail.com>
2023-11-11 23:07:26 -0600

    Initial Commit

---
+++ components/Logo.tsx
@@ -0,0 +1,24 @@
+import LogoImage from "@logos/black.svg";
+import Link from "next/link";
+import { AspectRatio } from "./ui/aspect-ratio";
+import Image from "next/image";
+
+function Logo() {
+  return <Link href="/" prefetch={false} className="overflow-hidden">
+    <div className="flex items-center w-72 h-14">
+      <AspectRatio
+      ratio={16 / 9}
+      className="flex items-center justify-center"
+      >
+        <Image 
+          priority
+          src={LogoImage}
+          alt="Anychat Logo"
+          className="dark:filter dark:invert"
+        />
+      </AspectRatio>
+    </div>
+  </Link>
+}
+
+export default Logo
---
+++ components/UserAvatar.tsx
@@ -0,0 +1,33 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+function UserAvatar({
+  name,
+  image,
+  className
+}: {
+  name?: string | null;
+  image?: string | null;
+  className?: string;
+}) {
+  return (
+    <Avatar className={cn('bg-white text-black', className)}>
+      {image && (
+        <Image
+          src={ image || "" } // To Do: Add default image url
+          alt={ name || "User Name" }
+          width={40}
+          height={40}
+          className="rounded-full"
+        />
+      )}
+      {/* <AvatarImage src="https://github.com/shadcn.png" /> */}
+      <AvatarFallback className="dark:bg-white dark:text-black text-lg" delayMs={1000}>
+        {name?.split(" ").map( n => n[0]).join("")}
+      </AvatarFallback>
+    </Avatar>
+  );
+}
+
+export default UserAvatar;
